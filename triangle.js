e203c73a47321d13c99aebe369e9f6b0c5295d82
Neel Parikh <nparikh@gmail.com>
2023-01-10 15:44:38 -0500

    Streamline the configuration of resizable traingle components

--- triangle.js
+++ triangle.js
@@ -1,111 +1,127 @@
-var draw = SVG().addTo('body').size(500, 500).css({ 'background-color': '#888' })
-    
-let xPos = 50
-let yMax = 300
-let yMin = 50
+"use strict";
+let draw = SVG().addTo('body').size(500, 500).css({ 'background-color': '#ddd' })
 
-let xMin = 50
-let xMax = 300
-let yPos = 300
 
-let lineWidth = 4
+let sizeMax = 300;
+let verts = [
+  [0, 0],
+  [sizeMax/2, 0],
+  [0, sizeMax/2],
+];
 
-let vertArray = [[xPos, yMax], [xPos, yMin], [yPos, xMax]]
-let triangle = draw.polygon(vertArray).fill('#9CE1FF')
+let triangle = draw.polygon(verts).fill('#9CE1FF');
 
-let rightAngleBox = draw.rect(15, 15)
-rightAngleBox.attr({
-  'fill-opacity': 0,
-  stroke: '#3a3a3c',
+let lineAttrs = {
+  width: 4,
+  linecap: 'round',
+};
+
+let lineA = draw.line([[0, 0], verts[1]]).stroke({
+  color: '#ff3b30',
+  ...lineAttrs,
+});
+
+let lineB = draw.line([[0, 0], verts[2]]).stroke({
+  color: '#007bff',
+  ...lineAttrs,
+});
+
+let lineC = draw.line([verts[1], verts[2]]).stroke({
+  color: '#34c759',
+  ...lineAttrs,
+});
+
+let circleAttrs = {
+  r: 5,
+  fill: "#fff",
+  stroke: "#000",
   'stroke-width': 1,
-})
-rightAngleBox.dx(xMin)
-rightAngleBox.dy(yPos - rightAngleBox.height())
+  style: "cursor: grab;"
+};
 
+let circleBeforeDragHandler = function(e) {
+  const { handler } = e.detail;
+  console.log(handler.el)
+  handler.el.css('cursor', 'grabbing');
+  draw.css('cursor', 'grabbing');
+}
 
-let lineC =  draw.line(xPos, yMin, xMax, yPos)
-lineC.stroke({ color: '#34c759', width: lineWidth, linecap: 'round' })
+let circleDragEndHandler = function(e) {
+  const { handler } = e.detail;
+  handler.el.css('cursor', 'grab');
+  draw.css('cursor', null);
+}
 
-
-let lineA = draw.line(xPos, yMin, xPos, yMax)
-lineA.stroke({ color: '#ff3b30', width: lineWidth, linecap: 'round' })
-
-let circleA = draw.circle(10).center(xPos, yMin).fill('white').stroke({width: 1, color: 'black'})
-circleA.draggable().css('cursor', 'grab')
-
-circleA.on('beforedrag.namespace', e => {
-  const { handler } = e.detail
-  handler.el.css('cursor', 'grabbing')
-  draw.css('cursor', 'grabbing')
-})
-
-circleA.on('dragend.namespace', e => {
-  const { handler, box } = e.detail
-  handler.el.css('cursor', 'grab')
-  draw.css('cursor', null)
-})
-
+let circleA = draw.circle(circleAttrs).center(...verts[1]).draggable();
+circleA.on('beforedrag.namespace', circleBeforeDragHandler);
+circleA.on('dragend.namespace', circleDragEndHandler);
 circleA.on('dragmove.namespace', e => {
   const { handler, box } = e.detail
   e.preventDefault()
-
-  let x = xPos
-
-  let y = box.cy;
-  if (box.cy <= yMin) {
-    y = yMin
-  }
-  if (box.cy >= yMax) {
-    y = yMax
-  }
-
-  handler.el.center(x, y)
-  lineA.attr('y1', y)
-  lineC.attr('y1', y)
-  vertArray[1] = [xPos, y]
-  triangle.plot(vertArray)
-})
-
-
-
-let lineB = draw.line(xMin, yPos, xMax, yPos)
-lineB.stroke({ color: '#007bff', width: lineWidth, linecap: 'round' })
-
-let circleB = draw.circle(10).center(xMax, yPos).fill('#fff').stroke({width: 1, color: 'black'})
-circleB.draggable().css('cursor', 'grab')
-
-circleB.on('beforedrag.namespace', e => {
-  const { handler } = e.detail
-  handler.el.css('cursor', 'grabbing').attr({})
-  draw.css('cursor', 'grabbing')
-})
-
-circleB.on('dragend.namespace', e => {
-  const { handler, box } = e.detail
-  handler.el.css('cursor', 'grab')
-  draw.css('cursor', null)
-})
-
-circleB.on('dragmove.namespace', e => {
-const { handler, box } = e.detail
-  e.preventDefault()
-
+  
   let x = box.cx;
-  if (box.cx <= xMin) {
-    x = xMin
+  if (box.cx <= 0) {
+    x = 0;
   }
-  if (box.cx >= xMax) {
-    x = xMax
+  else if (box.cx >= sizeMax) {
+    x = sizeMax;
   }
 
-  let y = yPos
-
-  handler.el.center(x, y)
-  lineB.attr('x2', x)
-  lineC.attr('x2', x)
-  vertArray[2] = [x, yPos]
-  triangle.plot(vertArray)
+  handler.el.center(x, 0)
+  lineA.attr('x2', x)
+  lineC.attr('x1', x)
+  verts[1] = [x, 0]
+  triangle.plot(verts)
 })
 
-circleA.front()
-circleB.front()
+let circleB = draw.circle(circleAttrs).center(...verts[2]).draggable();
+circleB.on('beforedrag.namespace', circleBeforeDragHandler);
+circleB.on('dragend.namespace', circleDragEndHandler);
+circleB.on('dragmove.namespace', e => {
+  const { handler, box } = e.detail
+  e.preventDefault()
+  
+  let y = box.cy;
+  if (box.cy <= 0) {
+    y = 0;
+  }
+  else if (box.cy >= sizeMax) {
+    y = sizeMax;
+  }
+
+  handler.el.center(0, y)
+  lineB.attr('y2', y)
+  lineC.attr('y2', y)
+  verts[2] = [0, y]
+  triangle.plot(verts)
+})
+
+
+
+// let rightAngleBox = draw.rect(15, 15)
+// rightAngleBox.attr({
+//   'fill-opacity': 0,
+//   stroke: '#3a3a3c',
+//   'stroke-width': 1,
+// })
+// rightAngleBox.dx(xMin)
+// rightAngleBox.dy(yPos - rightAngleBox.height())
+
+
+
+
+
+
+
+
+// class ResizableRightTriangle {
+//   constructor(width, height) {
+//     this.verts = [[0, 0], [width, 0], [0, height]];
+
+//     this.triangle = draw.polygon(this.verts).fill('#9CE1FF');
+    
+//     this.group = draw.group();
+//     this.group.add(this.triangle);
+//   }
+// }
+// triangle = new ResizableRightTriangle(300, 300);
