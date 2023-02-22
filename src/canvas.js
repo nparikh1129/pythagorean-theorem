6780c6c33fbbc8e8daa812a7b86d61ed83f1a22f
Neel Parikh <nparikh@gmail.com>
2023-02-22 11:20:01 -0500

    Add some basic css to canvas from file

--- src/canvas.js
+++ src/canvas.js
@@ -1,13 +1,15 @@
 export const draw = SVG()
   .addTo('body')
   .size(document.body.clientWidth, window.visualViewport.height)
-  .css({ 'background-color': '#1c1c1e' });
+draw.node.id = "canvas";
 
 // export const draw = SVG()
 //   .addTo('body')
 //   .attr({ width: "100%", height: "100%" })
 //   .css({ 'background-color': '#1c1c1e' })
-//   .viewbox(0, 0, 1200, 900);
+//   .viewbox(0, 0, 1200, 900)
+// draw.size(document.body.clientWidth, window.visualViewport.height)
+
 
 window.addEventListener("resize", () => {
   draw.size(document.body.clientWidth, window.visualViewport.height)
